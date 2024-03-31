fd368a7b82a606a681bacd6b446d7864d7410753
itumo-arigatone <soganokasegi0825@gmail.com>
2024-03-31 22:49:06 +0900

    can request api

--- js/getPR.js
+++ js/getPR.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
   console.log('loaded chrome extension');
   let description = document.querySelector('task-lists').firstElementChild.innerText;
-  chrome.storage.local.set({ 'prFirstLine': description });
+  chrome.storage.local.set({ 'prDescription': description });
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("aaauuuuuuuu");
-  chrome.storage.local.get('prFirstLine', function(data) {
-    document.getElementById('prFirstLine').textContent = data.prFirstLine;
+  document.getElementById('setting-button').addEventListener('click', function() {
+    console.log('clicked setting button')
+    chrome.runtime.openOptionsPage();
+  });
+
+  document.getElementById('review-button').addEventListener('click', function() {
+    console.log('clicked review button')
+    // request open ai api
+    chrome.storage.sync.get('apiKey', function(data) {
+      if (data === null || !data.apiKey) {
+        alert('API key is not set.');
+        return;
+      }
+      chrome.storage.local.get('prData', function(prData) {
+        let prDescription = prData.prDescription;
+        console.log(data.apiKey);
+
+        fetch('https://api.openai.com/v1/chat/completions', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${data.apiKey}`
+          },
+          body: JSON.stringify({
+            messages: [
+              { role: "system", content: "githubのPullRequestの説明文の評価をして修正点を指摘してください。" },
+              { role: "user", content: prDescription}
+            ],
+            model: "gpt-3.5-turbo",
+            temperature: 0.6, stream: false,
+          })
+        }).then(response => response.json())
+          .then(response_data => {
+            console.log(response_data);
+            if (response_data.error) {
+              document.getElementById('prDescription').textContent = response_data.error.message;
+            } else {
+              document.getElementById('prDescription').textContent = response_data.choices[0].text;
+            }
+          });
+      });
+    });
+  });
+  
+  chrome.storage.sync.get('apiKey', function(data) {
+    if (!data.apiKey) {
+      // only show setting button if API key is not set
+      document.getElementById('review-button').style.display = 'none';
+    }
+  });
+
+  chrome.storage.local.get('prDescription', function(data) {
+    document.getElementById('prDescription').textContent = 'revirew ressult';
   });
 });
